30ede1e674c9c01b5023310d945d201e762e4400
KM Rejoan Tanjim <kmrejoantanjim@gmail.com>
2025-10-02 19:38:32 +0000

    update devcontainer settings and add variable and data type examples

---
+++ 01.basic/01.variable.js
@@ -0,0 +1,24 @@
+const accountId = 123456
+let accountEmail= "rahat@gmail.com"
+var accountPassword = "12345"
+accoutCity = "dhaka"
+let accountState;
+
+// accountId = 12
+
+console.log(accountId);
+
+accountEmail = "khan@gmail.com"
+accountPassword = "2366564"
+accoutCity = "madaripur"
+
+console.table([accountId,accountEmail,accountPassword,accoutCity,accountState])
+
+
+/**notes: variable
+ * const = constant is not changeable
+ * prefer not to use var besause of issue in block scope and functional scope
+ */
+
+
+
---
+++ 01.basic/02.dataTypes.js
@@ -0,0 +1,37 @@
+"use strict"; // treat all JS code as newer version
+let name = "Rahat"
+let age = 20
+let isLoogedIn = false
+
+
+
+
+
+
+
+
+
+/**Note: dataType
+ * code readability that should ne high\
+ * **primitive dataType**
+ * number => 2 to power 53
+ * bigInt => for big number data
+ * string => "for string" 
+ * boolean => true/false
+ * null => standalone value "represent a empty value" | typeof: object
+ * undefined => value declered but not assign yet
+ * symbol => finding uniqueness 
+ * 
+ * 
+ * **non-premitive dataType**
+ * Object => {key:"value"}
+ * Array => [1,2,3,4] 
+ * function => function user(name,age){}
+ * Date => new Date()
+ * RegExp => /abc/
+ * map => new Map()
+ * set => new Set()
+ * weakMap => new WeakMap()
+ * WeakSet => new WeakSet()
+ * custom classes / instance => class Person {}
+ */
